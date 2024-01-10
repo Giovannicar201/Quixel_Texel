4f80cea495ca3e634e22214d9e36bff0e0d6d417
AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
2024-01-10 11:30:45 +0100

    Aggiunta di vari metodi in Validator.java.

--- src/main/java/Utility/Validator.java
+++ src/main/java/Utility/Validator.java
@@ -9,6 +9,7 @@ public class Validator {
     * REGEX E PATTERN GAC.
     *
     * */
+
     private static final String GMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@gmail.com$";
     private static final Pattern GMAIL_PATTERN = Pattern.compile(GMAIL_REGEX);
     private static final String YAHOO_REGEX = "^[a-zA-Z0-9._%+-]+@yahoo.com$";
@@ -28,6 +29,32 @@ public class Validator {
 
     private static final String NOME_CARTELLA_REGEX = "^(?=.*[a-zA-Z])[a-zA-Z]{1,32}$";
     private static final Pattern NOME_CARTELLA_PATTERN = Pattern.compile(NOME_CARTELLA_REGEX);
+
+    /*
+    *
+    * REGEX E PATTERN GEN.
+    *
+    * */
+
+    private static final String NOME_ENTITA_REGEX = "^[a-zA-Z]{1,32}$";
+    private static final Pattern NOME_ENTITA_PATTERN = Pattern.compile(NOME_ENTITA_REGEX);
+    private static final String COLLISIONE_REGEX = "\\b(?:si|no)\\b";
+    private static final Pattern COLLISIONE_PATTERN = Pattern.compile(COLLISIONE_REGEX);
+    private static final String NOME_PROPRIETA_REGEX = "^[a-zA-Z]{1,32}$";
+    private static final Pattern NOME_PROPRIETA_PATTERN = Pattern.compile(NOME_PROPRIETA_REGEX);
+    private static final String VALORE_PROPRIETA_REGEX = "^[a-zA-Z]{1,64}$";
+    private static final Pattern VALORE_PROPRIETA_PATTERN = Pattern.compile(VALORE_PROPRIETA_REGEX);
+
+    /*
+     *
+     * REGEX E PATTERN GMP.
+     *
+     * */
+
+    private static final String NOME_MAPPA_REGEX = "^[a-zA-Z]{1,32}$";
+    private static final Pattern NOME_MAPPA_PATTERN = Pattern.compile(NOME_MAPPA_REGEX);
+
+
 
     public static boolean isEmailValid(String email) {
         if(email == null)
@@ -61,4 +88,64 @@ public class Validator {
         return NOME_CARTELLA_PATTERN.matcher(nomeCartella).matches();
     }
 
+    public static boolean isPropertyNameValid(String nomeProprieta) {
+        if(nomeProprieta == null)
+            return false;
+
+        return NOME_PROPRIETA_PATTERN.matcher(nomeProprieta).matches();
+    }
+
+    public static boolean isPropertyValueValid(String valoreProprieta) {
+        if(valoreProprieta == null)
+            return false;
+
+        return VALORE_PROPRIETA_PATTERN.matcher(valoreProprieta).matches();
+    }
+
+    public static boolean isEntityNameValid(String nomeEntita) {
+        if(nomeEntita == null)
+            return false;
+
+        return NOME_ENTITA_PATTERN.matcher(nomeEntita).matches();
+    }
+
+    public static boolean isCollisionValid(String collisione) {
+        if(collisione == null)
+            return false;
+
+        return COLLISIONE_PATTERN.matcher(collisione).matches();
+    }
+
+    public static boolean isNumberOfPropertyValid(int numeroDiProprieta) {
+        return numeroDiProprieta >= 0 && numeroDiProprieta <= 4;
+    }
+
+    public static boolean isMapNameValid(String nomeMappa) {
+        if(nomeMappa == null)
+            return false;
+
+        return NOME_MAPPA_PATTERN.matcher(nomeMappa).matches();
+    }
+
+    public static boolean isMapWidthValid(long larghezza) {
+        return larghezza >= 0 && larghezza <= 32;
+    }
+
+    public static boolean isMapHeightValid(long altezza) {
+        return altezza >= 0 && altezza <= 32;
+    }
+
+    public static boolean isRowValid(String riga, long altezza) {
+        if(riga == null)
+            return  false;
+
+        return Integer.parseInt(riga) >= 0 && Integer.parseInt(riga) <= (altezza - 1);
+    }
+
+    public static boolean isColumnValid(String colonna, long larghezza) {
+        if(colonna == null)
+            return  false;
+
+        return Integer.parseInt(colonna) >= 0 && Integer.parseInt(colonna) <= (larghezza - 1);
+    }
 }
