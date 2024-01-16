0ae4f69f4f61be6e2e17ba9c8e3a9d820eba4aa6
Giovannicar201 <g.carbone42@studenti.unisa.it>
2024-01-17 00:26:24 +0100

    Aggiunta di classi Exception.

---
+++ src/main/java/QuixelTexel/IS/Exception/GPA/GPL/PaletteCreationException.java
@@ -0,0 +1,7 @@
+package QuixelTexel.IS.Exception.GPA.GPL;
+
+public class PaletteCreationException extends GPLException {
+    public PaletteCreationException(String message) {
+        super(message);
+    }
+}
