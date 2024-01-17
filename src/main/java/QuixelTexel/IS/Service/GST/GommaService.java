032bf28985f48fde2268d3e57a2a62d5a17faa99
AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
2024-01-17 14:48:04 +0100

    Aggiunta della classe GommaService.java.

---
+++ src/main/java/QuixelTexel/IS/Service/GST/GommaService.java
@@ -0,0 +1,24 @@
+package QuixelTexel.IS.Service.GST;
+
+import QuixelTexel.IS.Exception.GST.InvalidColumnException;
+import QuixelTexel.IS.Exception.GST.InvalidRowException;
+import QuixelTexel.IS.Service.CanvasService;
+import jakarta.transaction.Transactional;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class GommaService extends StrumentoService {
+
+    public GommaService(@Qualifier("mappaServiceImpl") CanvasService canvasService) {
+        super(canvasService);
+    }
+
+    @Override
+    @Transactional
+    public String usa(String canvas, String elemento, String riga, String colonna, String email)
+            throws InvalidColumnException,
+            ParseException,
+            InvalidRowException {
+        return canvasService.rimuovi(canvas,riga,colonna,email);
+    }
+}
