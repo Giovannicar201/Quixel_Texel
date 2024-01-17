458f69da3495f7df14923e3a17b523f8595cbb9d
AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
2024-01-17 15:00:14 +0100

    Modifica della classe IAControl.java.

--- src/main/java/QuixelTexel/IS/Control/GST/IAControl.java
+++ src/main/java/QuixelTexel/IS/Control/GST/IAControl.java
@@ -1,13 +1,13 @@
-package QuixelTexel.IS.Control.GMP.GST;
+package QuixelTexel.IS.Control.GST;
 
 import QuixelTexel.FIA.Service.IAServiceAdapter;
 import QuixelTexel.IS.Entity.GEN.GEN.EntitaEntity;
-import QuixelTexel.IS.Exception.GMP.GMP.GMPException;
+import QuixelTexel.IS.Exception.GST.GSTException;
 import QuixelTexel.IS.Exception.Session.MissingSessionEmailException;
 import QuixelTexel.IS.Exception.Session.MissingSessionMapException;
 import QuixelTexel.IS.Exception.Session.MissingSessionMapSelectionException;
 import QuixelTexel.IS.Service.GEN.GEN.EntitaService;
-import QuixelTexel.IS.Service.GMP.GST.MatitaService;
+import QuixelTexel.IS.Service.GST.MatitaServiceImpl;
 import QuixelTexel.IS.Utility.SessionManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,7 +16,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,15 +27,24 @@ import java.util.Base64;
 @Controller
 public class IAControl {
 
-    @Qualifier("matitaServiceMappaImpl")
     @Autowired
-    protected MatitaService matitaService;
+    protected MatitaServiceImpl matitaService;
     @Autowired
     protected EntitaService entitaService;
 
+    /**
+     * Gestisce la richiesta di generazione automatica di un individuo sulla mappa.
+     * L'individuo generato contiene entità posizionate in modo casuale tra i punti selezionati sulla mappa.
+     *
+     * @param request Oggetto HttpServletRequest che rappresenta la richiesta HTTP.
+     * @param response Oggetto HttpServletResponse che rappresenta la risposta HTTP.
+     * @throws GSTException Eccezione generica del gestore degli strumenti (GST).
+     * @see HttpServletRequest
+     * @see HttpServletResponse
+     * @see GSTException
+     */
     @RequestMapping(value = "/IA/genera", method = RequestMethod.POST)
-
-    public void genera(HttpServletRequest request, HttpServletResponse response) throws GMPException {
+    public void genera(HttpServletRequest request, HttpServletResponse response) throws GSTException {
 
         JSONParser parser = new JSONParser();
 
@@ -45,6 +53,7 @@ public class IAControl {
             String email = SessionManager.getEmail(request);
 
             String mappa = SessionManager.getMappa(request);
+
             JSONObject mappaJSON = (JSONObject) parser.parse(mappa);
             JSONArray entita = (JSONArray) mappaJSON.get("mappa");
 
@@ -93,14 +102,12 @@ public class IAControl {
 
             SessionManager.setMappa(request,mappaJSON.toString());
 
-            System.out.println(SessionManager.getMappa(request));
-
         } catch (ParseException | SQLException e) {
 
             try {
                 response.sendError(302, "NQTE");
             } catch (IOException ex) {
-                throw new GMPException("ERRORE - PARSEEXCEPTION || SQLEXCEPTION.");
+                throw new GSTException("ERRORE - PARSEEXCEPTION || SQLEXCEPTION.");
             }
 
         } catch (MissingSessionMapSelectionException e) {
@@ -108,7 +115,7 @@ public class IAControl {
             try {
                 response.sendError(302, "MSMSE");
             } catch (IOException ex) {
-                throw new GMPException("ERRORE - NESSUNA SELEZIONE MAPPA IN SESSIONE.");
+                throw new GSTException("ERRORE - NESSUNA SELEZIONE MAPPA IN SESSIONE.");
             }
 
         } catch (MissingSessionMapException e) {
@@ -116,7 +123,7 @@ public class IAControl {
             try {
                 response.sendError(302, "MSME");
             } catch (IOException ex) {
-                throw new GMPException("ERRORE - NESSUNA MAPPA IN SESSIONE.");
+                throw new GSTException("ERRORE - NESSUNA MAPPA IN SESSIONE.");
             }
 
         } catch (MissingSessionEmailException e) {
@@ -124,12 +131,9 @@ public class IAControl {
             try {
                 response.sendError(302, "MSEE");
             } catch (IOException ex) {
-                throw new GMPException("ERRORE - NESSUN UTENTE IN SESSIONE.");
+                throw new GSTException("ERRORE - NESSUN UTENTE IN SESSIONE.");
             }
 
         }
-
     }
-
 }
-
